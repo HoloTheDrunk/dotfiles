6a4672a3329f82c220056a95b2ea12bd815a2990
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-01-04 18:15:23 +0100

    feat(emacs): add .doom.d

---
+++ .doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mouse-wheel-progressive-speed nil)
+ '(package-selected-packages
+   '(clang-format+ flycheck-ghcmod weblio gnu-apl-mode python-docstring python-mode python-x rustic ecb)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
