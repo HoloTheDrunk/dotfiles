3faa3b754c284f9822eca29eb417436903f89cae
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-01-31 14:38:26 +0100

    feat(emacs): add slime and nasm-mode

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(mouse-wheel-progressive-speed nil)
  '(package-selected-packages
-   '(clang-format+ flycheck-ghcmod weblio gnu-apl-mode python-docstring python-mode python-x rustic ecb)))
+   '(clang-format+ slime nasm-mode flycheck-ghcmod weblio gnu-apl-mode python-docstring python-mode python-x rustic ecb)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
